0139d09fe72c9496ab17b9995464fabf5e06ceed
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-28 14:33:16 +0700

    membuat banner

---
+++ src/Components/Banner/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './banner.css'
+
+const Banner = () => {
+    return (
+        <section class="banner">
+            <h3>Sewa Mobil di (Lokasimu) Sekarang</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. </p>
+            <button className='button-hero'>Mulai Sewa Mobil</button>
+        </section>
+    )
+}
+
+export default Banner
